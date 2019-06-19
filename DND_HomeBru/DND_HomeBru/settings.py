5f5764e03da31f9310ac2ba2e968aea2265eb0ec
Lorenzo Gomez <lbg65@scarletmail.rutgers.edu>
2019-06-19 13:23:07 -0400

    We should isolate the models for each user

--- DND_HomeBru/DND_HomeBru/settings.py
+++ DND_HomeBru/DND_HomeBru/settings.py
@@ -33,13 +33,14 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'character_builder.apps.CharacterBuilderConfig',
     'users.apps.UsersConfig',
-    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #3rd party apps
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
