0f1c3b2381ab103521bced1df1265f064de3549b
Arax1 <anandraju55@hotmail.com>
2019-06-24 16:11:29 -0400

    applied user authentication to race update/delete

--- DND_HomeBru/character_builder/views.py
+++ DND_HomeBru/character_builder/views.py
@@ -4,11 +4,13 @@ from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Race, Class, Background, Character
-
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 # Create your views here.
 
 # homepage view
+
+
 def homepage(request):
     return render(request, "home.html", {'title': 'HomeBru - Home'})
 
@@ -23,17 +25,34 @@ class RaceListView(ListView):
 # view creating the races
 
 
-class RaceCreateView(CreateView):
+class RaceCreateView(LoginRequiredMixin, CreateView):
     model = Race
     template_name = "race_create_form.html"
-    success_url = reverse_lazy("home")
+    # success_url = reverse_lazy("home")
     fields = ['name', 'description', 'age', 'alignment', 'size', 'speed', ]
 
-class RaceUpdateView(UpdateView):
-    model =  Race
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class RaceUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Race
     template_name = 'race_update_form.html'
     # success_url = reverse_lazy('home')
     fields = ['name', 'description', 'age', 'alignment', 'size', 'speed', ]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        race = self.get_object()
+
+        if self.request.user == race.author:
+            return True
+
+        return False
 
 
 class RaceDetailView(DetailView):
@@ -41,10 +60,20 @@ class RaceDetailView(DetailView):
     context_object_name = "race_details"
     template_name = 'race_detail.html'
 
-class RaceDeleteView(DeleteView):
-    model  = Race
+
+class RaceDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Race
     template_name = 'race_delete_form.html'
-    success_url = reverse_lazy('home')
+    success_url = reverse_lazy('race_view')
+
+    def test_func(self):
+        race = self.get_object()
+
+        if self.request.user == race.author:
+            return True
+
+        return False
+
 
 class ClassListView(ListView):
     model = Class
@@ -52,14 +81,19 @@ class ClassListView(ListView):
     context_object_name = "classes"
 
 
-class ClassCreateView(CreateView):
+class ClassCreateView(LoginRequiredMixin, CreateView):
     model = Class
     template_name = "class_create_form.html"
     fields = ['name', 'description', 'hit_dice']
     success_url = reverse_lazy("home")
 
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
 class ClassUpdateView(UpdateView):
-    model =  Class
+    model = Class
     template_name = 'class_update_form.html'
     fields = ['name', 'description', 'hit_dice']
 
@@ -69,10 +103,12 @@ class ClassDetailView(DetailView):
     context_object_name = "class_details"
     template_name = 'class_detail.html'
 
+
 class ClassDeleteView(DeleteView):
-    model  = Class
+    model = Class
     template_name = 'class_delete_form.html'
     success_url = reverse_lazy('home')
+
 
 class BackgroundListView(ListView):
     model = Background
@@ -86,8 +122,9 @@ class BackgroundCreateView(CreateView):
     fields = ['name', 'description']
     success_url = reverse_lazy("home")
 
+
 class BackgroundUpdateView(UpdateView):
-    model =  Background
+    model = Background
     template_name = 'background_update_form.html'
     fields = ['name', 'description']
 
@@ -97,10 +134,12 @@ class BackgroundDetailView(DetailView):
     context_object_name = "background_details"
     template_name = 'background_detail.html'
 
+
 class BackgroundDeleteView(DeleteView):
-    model  = Background
+    model = Background
     template_name = 'background_delete_form.html'
     success_url = reverse_lazy('home')
+
 
 class CharacterCreateView(CreateView):
     model = Character
@@ -121,13 +160,15 @@ class CharacterDetailView(DetailView):
     template_name = 'character_detail.html'
     context_object_name = "character_details"
 
+
 class CharacterUpdateView(UpdateView):
-    model =  Character
+    model = Character
     template_name = 'character_update_form.html'
     fields = ['name', 'strength', 'intelligence', 'wisdom',
-          'constitution', 'dextirity', 'charisma', 'race_key']
+              'constitution', 'dextirity', 'charisma', 'race_key']
+
 
 class CharacterDeleteView(DeleteView):
-    model  = Character
+    model = Character
     template_name = 'character_delete_form.html'
     success_url = reverse_lazy('home')
